c2edbe3cbc541ccb7fd706cdeb81b8a81cf943b3
dumadul <166838394+dumadul@users.noreply.github.com>
2024-05-20 12:33:54 -0300

    Add files via upload

---
+++ musica.py
@@ -0,0 +1,38 @@
+musicas = [
+    ("Música 1", "Rock"),
+    ("Música 2", "rap de anime"),
+    ("Música 3", "sertanejo"),
+    ("Música 4", "trap"),
+    ("Música 5", "Pop"),
+]
+
+# Histórico de músicas ouvidas pelo usuário
+historico_usuario = ["Rock", "sertanejo", "rap de anime", "trap", "Pop"]
+
+
+# Função para recomendar músicas
+def recomendar_musica (historico):
+    # Contar a frequência de cada gênero no histórico
+    frequencia = {}
+    for genero in historico:
+        if genero in frequencia:
+           frequencia [genero] += 1
+        else:
+           frequencia [genero] = 1
+    
+    
+    # Encontrar o gênero mais frequente
+    genero_mais_frequente = max(frequencia, key=frequencia.get)
+    
+    
+    # Recomendar músicas desse gênero
+    recomendacoes = []
+    for titulo, genero in musicas:
+        if genero == genero_mais_frequente:
+            recomendacoes.append(titulo)
+    return recomendacoes
+    
+    
+# Obter recomendações para o usuário
+recomendacoes_usuario = recomendar_musica (historico_usuario)
+print("Músicas recomendadas:", recomendacoes_usuario)
